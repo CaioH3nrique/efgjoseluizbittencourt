7eb7b173e2f3135481145a85c12269dc832a1da3
C4lO <caiohenrique9705@gmail.com>
2025-08-08 16:15:09 -0300

    aula 08/06

---
+++ python.py
@@ -0,0 +1,5 @@
+nome = input('qual o seu nome?')
+idade = input('qual é sua idade?')
+cidade = input('qual a sua cidade?')
+
+print(f"boas vindas {nome}, sua idade é {idade} anos e você é de {cidade}.")
